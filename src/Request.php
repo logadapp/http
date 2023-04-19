7506fa20e671678cac5cd3ad13c0e8f1ac91baf8
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-04-19 16:13:49 +0100

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -10,52 +10,80 @@ namespace Logadapp\Http;
 
 use Rakit\Validation\Validator;
 
-class Request
+final class Request
 {
+    private string $uri;
+    private string $method;
+    private array $headers;
+    // private array $serverData;
     private array $get;
     private array $post;
     private array $files;
-    private array $args;
     private string $rawBody;
-    /**
-     * @var mixed|null
-     */
-    private $path;
     private string $validationError;
 
-    public function __construct(array $get = array(), array $post = array(), array $files = array(), string $rawBody = '')
+    public function __construct(array $get = [], array $post = [], array $files = [], string $rawBody = '', array $headers = [])
     {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->uri = $_SERVER['REQUEST_URI'];
         $this->get = $get;
         $this->post = $post;
         $this->files = $files;
-        // $this->rawBody = file_get_contents('php://input');
         $this->rawBody = $rawBody;
+        // $this->serverData = $serverData;
+        // $this->headers = $this->setHeadersFromServerData($serverData);
+        $this->headers = $headers;
     }
 
+    /*private function setHeadersFromServerData(array $serverData): array
+    {
+        $headers = [];
+        foreach ($serverData as $key => $value) {
+            if (str_starts_with($key, 'HTTP_')) {
+                $headers[str_replace('HTTP_', '', $key)] = $value;
+            }
+        }
+        return $headers;
+    }*/
 
-    public function getPost()
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function getContentType(): string
+    {
+        return $this->getHeader('Content-Type');
+    }
+
+    public function getQueryParams(string $name = null): array|string
+    {
+        return is_null($name) ? $this->get : $this->get[$name] ?? '';
+    }
+
+    public function getHeaders():array
+    {
+        return $this->headers;
+    }
+
+    public function getHeader(string $name, string $default = ''): string
+    {
+        return $this->headers[$name] ?? $default;
+    }
+
+    public function getBody(): array
+    {
+        return $this->getContentType() == 'application/json' ? json_decode($this->rawBody, true) : $this->getPost();
+    }
+
+    public function getPost():array
     {
         return $this->post;
-    }
-
-    public function getQuery(string $queryName = ''): array|string
-    {
-        return !empty($queryName) ? ($this->query[$queryName] ?? '') : $this->get;
-    }
-
-    public function getArguments():array
-    {
-        return $this->args;
-    }
-
-    public function setPath(string $path): self
-    {
-        $this->path = $path;
-        return $this;
-    }
-    public function getPath(): string
-    {
-        return $this->path;
     }
 
     public function getFiles():array
@@ -68,14 +96,15 @@ class Request
         return $this->rawBody;
     }
 
-    public function getParsedBody()
+    /*public function getParsedBody()
     {
         if (function_exists('cleanBody')) {
-            return cleanBody($this->getPOSTBody());
+            return cleanBody($this->getBody());
         }
         return $this;
-    }
-    public function validate($data, array $rules)
+    }*/
+
+    public function validate(array $data, array $rules):bool
     {
         $validator = new Validator;
         $validator->setMessages([
@@ -90,15 +119,15 @@ class Request
                 $errorMessage .= "$errMsg, ";
             }
             $errorMessage = rtrim($errorMessage, ', ');
-            $this->setValidationError($errorMessage);
+            $this->validationError = $errorMessage;
             return false;
         } else {
             return true;
         }
     }
 
-    private function setValidationError(string $errorMessage)
+    public function getValidationError():string
     {
-        $this->validationError = $errorMessage;
+        return $this->validationError;
     }
 }
