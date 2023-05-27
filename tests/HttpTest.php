7bc006e8c6968e9996683aa85a9a21f3121f5083
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-05-27 08:11:01 +0100

    Added more tests

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -75,88 +75,65 @@ class HttpTest extends TestCase
         $this->assertEquals($headers, $retrievedHeaders);
     }
 
-    public function testSetUrl()
+    public function testGet()
     {
-        $
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
+        $url = 'https://example.com';
+        $http = Http::get($url);
 
-        $this->assertIsString($http->getUrl());
+        $this->assertInstanceOf(Http::class, $http);
+        $this->assertEquals($url, $http->getUrl());
+        $this->assertEquals('GET', $http->getMethod());
+        $this->assertEquals('', $http->getBody());
+        $this->assertEquals([], $http->getHeaders());
     }
 
-    public function testGetResponseHeaders()
+    public function testGetWithHeaders()
     {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
+        $url = 'https://example.com';
+        $headers = ['foo: bar'];
 
+        $http = Http::get($url)
+            ->setHeaders($headers);
+
+        $this->assertInstanceOf(Http::class, $http);
+        $this->assertEquals($url, $http->getUrl());
+        $this->assertEquals('GET', $http->getMethod());
+        $this->assertEquals($headers, $http->getHeaders());
+    }
+
+    public function testPost()
+    {
+        $url = 'https://example.com';
+        $body = json_encode([
+            'name' => 'Michael'
+        ]);
+        $headers = ['Content-Type: application/json'];
+
+        $http = Http::post($url)
+            ->setBody($body)
+            ->setHeaders($headers);
+
+        $this->assertInstanceOf(Http::class, $http);
+        $this->assertEquals($url, $http->getUrl());
+        $this->assertEquals('POST', $http->getMethod());
+        $this->assertEquals($body, $http->getBody());
+        $this->assertEquals($headers, $http->getHeaders());
+    }
+
+    public function testSend()
+    {
+        $url = 'https://webhook.site/802d2f8d-1ffd-4679-9013-5d6ca29fb263';
+        $body = json_encode([
+            'name' => 'Michael'
+        ]);
+        $headers = ['Content-Type: application/json'];
+
+        $http = Http::post($url)
+            ->setBody($body)
+            ->setHeaders($headers)
+            ->send();
+
+        $this->assertInstanceOf(Http::class, $http);
         $this->assertIsArray($http->getResponseHeaders());
-    }
-
-    public function testGetResponseBody()
-    {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
-
-        $this->assertIsString($http->getResponseBody());
-    }
-
-    public function testSetTimeout()
-    {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
-
-        $this->assertIsInt($http->getTimeout());
-    }
-
-    public function testSetMethod()
-    {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
-
-        $this->assertIsString($http->getMethod());
-    }
-
-    public function testSetUrl()
-    {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
-
-        $this->assertIsString($http->url);
-    }
-
-    public function testGetBody()
-    {
-        $http = Http::build([
-            'url' => 'https://www.logad.net',
-            'method' => 'GET',
-            'body' => '',
-            'headers' => []
-        ]);
-
-        $this->assertIsString($http->body);
     }
 }
